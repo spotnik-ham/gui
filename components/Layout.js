8afcc595a695a9edb3de48566d08f638c8c26371
f4gbv <f4gbv@yahoo.fr>
2018-11-28 18:11:12 +0100

    icone rotation issue fixed
    correction pour la rotation permanente de l'icone spotnik
    88 de isa:)

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header'
 
+
 const Layout = props => (
 	<div>
 		<Header/>
 		<div className="content">
+			<h1><center><img className="logo1" src="../static/spotnik.svg" /> Dashboard du <a target="_blank" href="https://f5nlg.wordpress.com/2015/12/28/nouveau-reseau-french-repeater-network/">Réseau des Répéteurs Francophones (RRF) <img className="logo2" src="../static/spotnik.svg" /> </a></center></h1>
+			<audio src="http://rrf.f5nlg.ovh:8000/stream" controls></audio>
 			{props.children}
 		</div>
 		<style jsx>{`
@@ -12,8 +15,28 @@ const Layout = props => (
 				padding: 15px;
 				padding-top: 70px;
 			}
-		`}</style>
+
+                        @keyframes tourne1 {
+                            from { transform: rotate(0deg); }
+                            to { transform: rotate(360deg); }
+                        }
+
+                        @keyframes tourne2 {
+                            from { transform: rotate(180deg); }
+                            to { transform: rotate(-180deg); }
+                        }
+
+                        .logo1 {
+                            height: 50px;
+                            animation: tourne1 12s linear 0s infinite;
+                        }
+
+                        .logo2 {
+                            height: 50px;
+                            animation: tourne2 12s linear 0s infinite;
+                        }
+
+		}</style>
 	</div>
 )
-
 export default Layout
