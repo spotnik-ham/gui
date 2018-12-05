e835510bde6e27012ed019a00c321a5520fda5f5
f4gbv <f4gbv@yahoo.fr>
2018-12-05 18:41:34 +0100

    update DashBoard (list to array)
    mise-à-jour formatage du dashboard :
    passage du format liste déroulante au format tableau,
    de manière à visualiser plus rapidement la totalité des links.

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,26 @@
 import Head from 'next/head'
 import React from 'react'
-
 import Navigation from './Navigation'
+
+
+
 
 const Header = () => (
 	<div>
 		<Head>
-			<title>RRF - Réseau des Répéteurs Francophone</title>
+			<title>spotnik</title>
 			<meta charSet="utf-8"/>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-			<link rel="icon" type="image/png" href="../static/logo.png"/>
+			<link rel="icon" type="image/png" href="../static/favicon.png"/>
 			{/* FIXME don't know how to webpack bundle this */}
 			{/* <link href="notie/dist/notie.min.css" rel="stylesheet" type="text/css"/> */}
 			<link href="../static/notie.min.css" rel="stylesheet" type="text/css"/>
 			<link href="../static/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 			<script src="../static/jquery.slim.min.js"/>
 			<script src="../static/bootstrap.min.js"/>
+			
 		</Head>
+		<Navigation/>
 	</div>
 )
 
