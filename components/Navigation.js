ec5e57d75bf6e17715c05a5d7e134e257d593413
Sonny Piers <sonny@fastmail.net>
2019-08-21 19:58:50 +0200

    remove deprecated prefetch attribute on links
    https://github.com/zeit/next.js/blob/master/errors/prefetch-true-deprecated.md

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,50 +4,72 @@ import React from 'react'
 const Navigation = () => {
 	return (
 		<nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
-			<button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"/>
+			<button
+				className="navbar-toggler navbar-toggler-right"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon" />
 			</button>
-			<Link prefetch href="/">
-				<a className="navbar-brand"><img src="../static/spotnik.svg" height="30"/> spotnik</a>
+			<Link href="/">
+				<a className="navbar-brand">
+					<img src="../static/spotnik.svg" height="30" /> spotnik
+				</a>
 			</Link>
 			<div className="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul className="navbar-nav mr-auto">
 					<li className="nav-item">
-						<Link prefetch href="/">
-							<a className="nav-link"><img src="../static/home.svg" height="14"/> home</a>
+						<Link href="/">
+							<a className="nav-link">
+								<img src="../static/home.svg" height="14" /> home
+							</a>
 						</Link>
 					</li>
 					<li className="nav-item">
-						<Link prefetch href="/keypad">
-							<a className="nav-link"><img src="../static/keypad.svg" height="14"/> keypad</a>
-						</Link>
-					</li>
-					
-					<li className="nav-item">
-						<Link prefetch href="/configuration">
-							<a className="nav-link"><img src="../static/configuration.svg" height="14"/> configuration</a>
+						<Link href="/keypad">
+							<a className="nav-link">
+								<img src="../static/keypad.svg" height="14" /> keypad
+							</a>
 						</Link>
 					</li>
 
 					<li className="nav-item">
-						<Link prefetch href="/status">
-							<a className="nav-link"><img src="../static/status.svg" height="14"/> status</a>
+						<Link href="/configuration">
+							<a className="nav-link">
+								<img src="../static/configuration.svg" height="14" />{' '}
+								configuration
+							</a>
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link href="/status">
+							<a className="nav-link">
+								<img src="../static/status.svg" height="14" /> status
+							</a>
 						</Link>
 					</li>
 					<li className="nav-item">
-						<Link prefetch href="/admin">
-							<a className="nav-link"><img src="../static/admin.svg" height="14"/> admin</a>
+						<Link href="/admin">
+							<a className="nav-link">
+								<img src="../static/admin.svg" height="14" /> admin
+							</a>
 						</Link>
 					</li>
 					<li className="nav-item">
-						<Link prefetch href="/about">
-							<a className="nav-link"><img src="../static/about.svg" height="14"/> about</a>
+						<Link href="/about">
+							<a className="nav-link">
+								<img src="../static/about.svg" height="14" /> about
+							</a>
 						</Link>
 					</li>
 				</ul>
 			</div>
-			<style jsx>{`
-			`}</style>
+			<style jsx>{``}</style>
 		</nav>
 	)
 }
