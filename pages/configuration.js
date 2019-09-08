545f6169f448baad6f1d81675a88334c891a9ac5
f1evm <github@f1evm.fr>
2019-09-09 00:28:22 +0200

    Ajout de l'option 'H' pour le type de node

--- pages/configuration.js
+++ pages/configuration.js
@@ -22,52 +22,53 @@ class Component extends React.Component {
 
 	static getInitialProps() {
 		return fetch('/api/configuration').then(res => res.json()).then(config => {
-			return {config}
+			return { config }
 		})
 	}
 
 	handleGetLocation() {
 		notie.info('Looking for location')
 		navigator.geolocation.getCurrentPosition(
-			({coords}) => {
+			({ coords }) => {
 				notie.info('Location acquired')
 				const [latitude, longitude] = formatcoords(coords.latitude, coords.longitude)
-					.format('D.M.sX', {decimalPlaces: 0})
+					.format('D.M.sX', { decimalPlaces: 0 })
 					.split(' ')
-				this.setState({location_latitude: latitude, location_longitude: longitude}) // eslint-disable-line camelcase
+				this.setState({ location_latitude: latitude, location_longitude: longitude }) // eslint-disable-line camelcase
 			},
 			err => notie.error(err),
-			{enableHighAccuracy: true}
+			{ enableHighAccuracy: true }
 		)
 	}
 
-	handleLocationChange({target}) {
-		this.setState({location_enabled: target.checked}) // eslint-disable-line camelcase
+	handleLocationChange({ target }) {
+		this.setState({ location_enabled: target.checked }) // eslint-disable-line camelcase
 	}
 
-	handleChange({target}) {
-		this.setState({[target.name]: target.value})
+	handleChange({ target }) {
+		this.setState({ [target.name]: target.value })
 	}
 
 	handleSubmit(event) {
 		event.preventDefault()
 		const headers = new Headers()
 		headers.append('Content-Type', 'application/json')
-		fetch('/api/configuration', {method: 'POST', headers, body: JSON.stringify(this.state)})
+		fetch('/api/configuration', { method: 'POST', headers, body: JSON.stringify(this.state) })
 			.then(() => notie.success('Saved. Restarting SvxLink...'))
-			.catch(() => {})
+			.catch(() => { })
 	}
 
 	render() {
 		const value = prop => this.state[prop]
 		const band_typ = [
-		'10M',
-		'V',
-		'U',
-		'T',
-		'S',
-		'R',
-		'6M'
+			'6M',
+			'10M',
+			'H',
+			'V',
+			'U',
+			'T',
+			'S',
+			'R'
 		]
 		this.lSA818 = 'No'
 
@@ -81,7 +82,7 @@ class Component extends React.Component {
 		//	'183.5','189.9','196.6','203.5','210.7','218.1','225.7','233.6','241.8',
 		//	'250.3'
 		//]
-	
+
 		// nouveau tableau des ctcss compatible avec SA818 :
 		// la correspondance dans SA818 est la suivante : numéro d'élément sur 4 chiffres
 		// exemple : le dernier élément du tableau est 0038
@@ -93,18 +94,18 @@ class Component extends React.Component {
 			'241.8', '250.3'
 		]
 		const sql_lvl = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
-		const lSA818 = ['No','Yes']
+		const lSA818 = ['No', 'Yes']
 
 		return (
 			<Layout>
 				<form onSubmit={this.handleSubmit}>
 					<div className="form-group">
 						<label htmlFor="callsign">Call sign</label>
-						<input required placeholder="5P07N1K" type="text" className="form-control" name="callsign" value={value('callsign')} onChange={this.handleChange}/>
+						<input required placeholder="5P07N1K" type="text" className="form-control" name="callsign" value={value('callsign')} onChange={this.handleChange} />
 					</div>
 					<div className="form-group">
-							<label htmlFor="Departement">Departement</label>
-							<input placeholder="22" type="text" className="form-control" name="Departement" value={value('Departement')} onChange={this.handleChange}/>
+						<label htmlFor="Departement">Departement</label>
+						<input placeholder="22" type="text" className="form-control" name="Departement" value={value('Departement')} onChange={this.handleChange} />
 					</div>
 					<div className="form-group">
 						<label htmlFor="type">Node type</label>
@@ -114,10 +115,10 @@ class Component extends React.Component {
 						</select>
 					</div>
 					<div className="form-group">
-							<label htmlFor="band_type">Frequency Band  ( V=VHF, D=10m, T=Tspdr, R=Relais ...)</label>
-							<select required name="band_type" className="form-control" value={value('band_type')} onChange={this.handleChange}>
-								{band_typ.map(band => <option key={band} value={band}>{band}</option>)}
-							</select>
+						<label htmlFor="band_type">Frequency Band  ( V=VHF, D=10m, T=Tspdr, R=Relais ...)</label>
+						<select required name="band_type" className="form-control" value={value('band_type')} onChange={this.handleChange}>
+							{band_typ.map(band => <option key={band} value={band}>{band}</option>)}
+						</select>
 					</div>
 					<div className="form-group">
 						<label htmlFor="default_lang">Language</label>
@@ -148,16 +149,16 @@ class Component extends React.Component {
 						<legend>Location</legend>
 						<div className="form-group">
 							<label className="form-check-label">
-								<input className="form-check-input" type="checkbox" onChange={this.handleLocationChange} checked={value('location_enabled') ? 'checked' : ''}/> Enable
+								<input className="form-check-input" type="checkbox" onChange={this.handleLocationChange} checked={value('location_enabled') ? 'checked' : ''} /> Enable
 							</label>
 						</div>
 						<div className="form-group">
 							<label htmlFor="location_latitude">latitude</label>
-							<input placeholder="55.48.58S" type="text" className="form-control" name="location_latitude" value={value('location_latitude')} onChange={this.handleChange}/>
+							<input placeholder="55.48.58S" type="text" className="form-control" name="location_latitude" value={value('location_latitude')} onChange={this.handleChange} />
 						</div>
 						<div className="form-group">
 							<label htmlFor="location_longitude">longitude</label>
-							<input placeholder="11.15.00E" type="text" className="form-control" name="location_longitude" value={value('location_longitude')} onChange={this.handleChange}/>
+							<input placeholder="11.15.00E" type="text" className="form-control" name="location_longitude" value={value('location_longitude')} onChange={this.handleChange} />
 						</div>
 						<p>
 							<button type="button" onClick={this.handleGetLocation}>Get automatic location</button> from
@@ -168,20 +169,20 @@ class Component extends React.Component {
 						<legend>EchoLink</legend>
 						<div className="form-group">
 							<label htmlFor="echolink_password">password</label>
-							<input placeholder="password" type="password" className="form-control" name="echolink_password" value={value('echolink_password')} onChange={this.handleChange}/>
+							<input placeholder="password" type="password" className="form-control" name="echolink_password" value={value('echolink_password')} onChange={this.handleChange} />
 						</div>
 						<p>See <a href="http://www.echolink.org/validation/">validation</a></p>
 						<div className="form-group">
 							<label htmlFor="echolink_password">proxy server</label>
-							<input placeholder="example.com" type="text" className="form-control" name="echolink_proxy_server" value={value('echolink_proxy_server')} onChange={this.handleChange}/>
+							<input placeholder="example.com" type="text" className="form-control" name="echolink_proxy_server" value={value('echolink_proxy_server')} onChange={this.handleChange} />
 						</div>
 						<div className="form-group">
 							<label htmlFor="echolink_proxy_port">proxy port</label>
-							<input placeholder="8100" type="number" className="form-control" name="echolink_proxy_port" value={value('echolink_proxy_port')} onChange={this.handleChange}/>
+							<input placeholder="8100" type="number" className="form-control" name="echolink_proxy_port" value={value('echolink_proxy_port')} onChange={this.handleChange} />
 						</div>
 						<div className="form-group">
 							<label htmlFor="echolink_proxy_password">proxy password</label>
-							<input placeholder="password" type="password" className="form-control" name="echolink_proxy_password" value={value('echolink_proxy_password')} onChange={this.handleChange}/>
+							<input placeholder="password" type="password" className="form-control" name="echolink_proxy_password" value={value('echolink_proxy_password')} onChange={this.handleChange} />
 						</div>
 						<p>See <a href="http://www.echolink.org/proxylist.jsp">proxy list</a></p>
 					</fieldset>
@@ -189,15 +190,15 @@ class Component extends React.Component {
 						<legend>Propagation alerts</legend>
 						<div className="form-group">
 							<label htmlFor="mail_server">Mail server</label>
-							<input placeholder="imap.example.com" type="text" className="form-control" name="mail_server" value={value('mail_server')} onChange={this.handleChange}/>
+							<input placeholder="imap.example.com" type="text" className="form-control" name="mail_server" value={value('mail_server')} onChange={this.handleChange} />
 						</div>
 						<div className="form-group">
 							<label htmlFor="mail_username">Mail address</label>
-							<input placeholder="username@example.com" type="email" className="form-control" name="mail_username" value={value('mail_username')} onChange={this.handleChange}/>
+							<input placeholder="username@example.com" type="email" className="form-control" name="mail_username" value={value('mail_username')} onChange={this.handleChange} />
 						</div>
 						<div className="form-group">
 							<label htmlFor="mail_password">Mail password</label>
-							<input placeholder="password" type="password" className="form-control" name="mail_password" value={value('mail_password')} onChange={this.handleChange}/>
+							<input placeholder="password" type="password" className="form-control" name="mail_password" value={value('mail_password')} onChange={this.handleChange} />
 						</div>
 						<p>See <a href="https://f5nlg.wordpress.com/2017/05/29/doc-du-module-propagation-monitor/">documentation</a></p>
 					</fieldset>
@@ -205,31 +206,31 @@ class Component extends React.Component {
 						<legend>Meteo information</legend>
 						<div className="form-group">
 							<label htmlFor="airport_code">Airport ICAO code</label>
-							<input placeholder="LFRO" type="text" className="form-control" name="airport_code" value={value('airport_code')} onChange={this.handleChange}/>
+							<input placeholder="LFRO" type="text" className="form-control" name="airport_code" value={value('airport_code')} onChange={this.handleChange} />
 						</div>
 						<p>See <a href="http://fr.allmetsat.com/metar-taf/france.php">airport codes</a></p>
 					</fieldset>
 					<fieldset className="form-group">
-                        <legend>Wifi</legend>
-                        <div className="form-group">
-                            <label htmlFor="wifi_ssid">ssid</label>
-                            <input placeholder="ssid" type="text" className="form-control" name="wifi_ssid" value={value('wifi_ssid')} onChange={this.handleChange}/>
-                        </div>
-				        <div className="form-group">
-                            <label htmlFor="wpa_key">wpa_key</label>
-                            <input placeholder="clef wpa" type="text" className="form-control" name="wpa_key" value={value('wpa_key')} onChange={this.handleChange}/>
-                        </div>
+						<legend>Wifi</legend>
+						<div className="form-group">
+							<label htmlFor="wifi_ssid">ssid</label>
+							<input placeholder="ssid" type="text" className="form-control" name="wifi_ssid" value={value('wifi_ssid')} onChange={this.handleChange} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="wpa_key">wpa_key</label>
+							<input placeholder="clef wpa" type="text" className="form-control" name="wpa_key" value={value('wpa_key')} onChange={this.handleChange} />
+						</div>
 					</fieldset>
 					<fieldset className="form-group">
 						<legend>SA818 (for spotnik delta with integrated radio only) </legend>
 						<div className="form-group">
-                            <label htmlFor="Tx_qrg">tx_qrg</label>
-                            <input placeholder="145.3250" type="text" className="form-control" name="tx_qrg" value={value('tx_qrg')} onChange={this.handleChange}/>
-                        </div>
+							<label htmlFor="Tx_qrg">tx_qrg</label>
+							<input placeholder="145.3250" type="text" className="form-control" name="tx_qrg" value={value('tx_qrg')} onChange={this.handleChange} />
+						</div>
 						<div className="form-group">
-                            <label htmlFor="Rx_qrg">rx_qrg</label>
-                            <input placeholder="432.9000" type="text" className="form-control" name="rx_qrg" value={value('rx_qrg')} onChange={this.handleChange}/>
-                        </div>
+							<label htmlFor="Rx_qrg">rx_qrg</label>
+							<input placeholder="432.9000" type="text" className="form-control" name="rx_qrg" value={value('rx_qrg')} onChange={this.handleChange} />
+						</div>
 						<div className="form-group">
 							<label htmlFor="sql_lvl">sql_lvl</label>
 							<select required name="sql_lvl" className="form-control" value={value('sql_lvl')} onChange={this.handleChange}>
@@ -238,13 +239,13 @@ class Component extends React.Component {
 						</div>
 						<div className="form-group">
 							<label htmlFor="lSA818">Are you sure you wish to program SA818 now by clicking on SAVE ?</label>
-							<select required name="lSA818" className="form-control" value={value('l_818')}  onChange={this.handleChange}>
+							<select required name="lSA818" className="form-control" value={value('l_818')} onChange={this.handleChange}>
 								{lSA818.map(lSA818 => <option key={lSA818} value={lSA818}>{lSA818}</option>)}
 							</select>
-							
-					</div>
+
+						</div>
 					</fieldset>
-					<input type="submit" className="btn btn-primary" value="Save"/>
+					<input type="submit" className="btn btn-primary" value="Save" />
 				</form>
 			</Layout>
 		)
