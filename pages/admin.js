f660336eb4e7f0ca476847935b448e5041920714
f1evm <github@f1evm.fr>
2020-05-22 10:23:43 +0200

    update

--- pages/admin.js
+++ pages/admin.js
@@ -45,7 +45,7 @@ class Component extends React.Component {
 		try {
 			var gV = {}
 			console.log('getVersions')
-			gV = await fetch('/update').catch(err => console.error(err))
+			//gV = await fetch('/update').catch(err => console.error(err))
 			//var gV = "gV"
 			console.log("getVersions : ", gV)
 			this.setState({ versions: gV })
